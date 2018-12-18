2e63b71934920fb7c37ec4e8041b68ff4f0b55ae
DeexIsTheBest <45711286+DeexIsTheBest@users.noreply.github.com>
2018-12-18 13:35:22 +0000

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`:rose:  ولكم نورت السيرفر:rose: 
+:crown: ${member}:crown:  
+انت العضو رقم ${member.guild.memberCount} `) 
+}).catch(console.error)
+})
+
+client.login(process.env.BOT_TOKEN);
